ae65319d225ae5a81ad5775490bd06db3e9c155f
Jake Soenneker <jake@soenneker.com>
2024-01-04 08:45:01 -0600

    ConfigureAwait

--- src/RedisServerClient.cs
+++ src/RedisServerClient.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Soenneker.Extensions.ValueTask;
 using Soenneker.Redis.Client.Abstract;
 using Soenneker.Redis.Client.Server.Abstract;
 using Soenneker.Utils.AsyncSingleton;
@@ -18,12 +19,12 @@ public class RedisServerClient : IRedisServerClient
     {
         _client = new AsyncSingleton<IServer>(async () =>
         {
-            logger.LogDebug(">> RedisServerClient: Building IServer from multiplexor...");
+            logger.LogDebug(">> RedisServerClient: Building IServer from multiplexer...");
 
-            ConnectionMultiplexer connectionMultiplexor = await redisClient.GetClient();
+            ConnectionMultiplexer connectionMultiplexer = await redisClient.GetClient().NoSync();
 
-            EndPoint[] endpoints = connectionMultiplexor.GetEndPoints();
-            IServer client = connectionMultiplexor.GetServer(endpoints[0]);
+            EndPoint[] endpoints = connectionMultiplexer.GetEndPoints();
+            IServer client = connectionMultiplexer.GetServer(endpoints[0]);
 
             return client;
         });
